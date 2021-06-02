81f5be3e8558cfb963c848bbe1dcf93538e3b1fa
yango3 <o3.yang@hotmail.com>
2021-06-02 16:48:26 +0800

    update react test file

--- tutorial-prj/react-test/app/hello.js
+++ tutorial-prj/react-test/app/hello.js
@@ -3,11 +3,16 @@ import React from 'react'
 // var HelloWorld = React.createClass({
 class HelloWorld extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {name:"Default Name"}
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello World!</h1>
-                <h2 className="subheading">Hello</h2>
+                <h2 className="subheading">{this.props.name}</h2>
             </div>
         );
     }
--- tutorial-prj/react-test/test/test.js
+++ tutorial-prj/react-test/test/test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 //var React = require('react')
 // import TestUtils from 'react-addons-test-utils';
 // var ReactTestUtils = require('react-addons-test-utils') // ES5 with npm
@@ -17,9 +18,30 @@ describe("renderIntoDocument", () => {
         //  This is because 'renderIntoDocument' renders into a detached DOM node.
         let element = ReactTestUtils.renderIntoDocument(<HelloWorld />);
         expect(element).toBeTruthy();
+        pending("Render into OK")
     });
 
-    // afterAll(() => {
+    it("should render the component and it's html into a dom node", ()=>{
+        let myComponent = ReactTestUtils.renderIntoDocument(<HelloWorld />)
 
-    // });
+        expect(ReactDOM.findDOMNode(myComponent).textContent).toContain("Hello World!")
+    });
+    it("should render the component and return the component as the return value", ()=>{
+        let myComponent = ReactTestUtils.renderIntoDocument(<HelloWorld name="Bleeding"/>)
+
+        expect(myComponent.props.name).toBe("Bleeding")
+    })
+    it("should not put the component into the DOM", ()=>{
+        let myComponent = ReactTestUtils.renderIntoDocument(<HelloWorld />)
+
+        expect(ReactDOM.findDOMNode(myComponent).offsetWidth).toBe(0)
+        expect(ReactDOM.findDOMNode(myComponent).offsetHeight).toBe(0)
+    });
+    it("can have finder methods run against it", ()=>{
+        let myComponent = ReactTestUtils.renderIntoDocument(<HelloWorld name="Bleading"/>)
+
+        let mySubheading = ReactTestUtils.findRenderedDOMComponentWithClass(myComponent, "subheading");
+
+        expect(ReactDOM.findDOMNode(mySubheading).innerHTML).toBe("Bleading")
+    })
 });
