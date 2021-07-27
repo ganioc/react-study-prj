478ea27d39b9d2a2c1e463769aafb87e660a6a39
yango3 <o3.yang@hotmail.com>
2021-07-27 11:51:18 +0800

    update typescript react

--- tutorial-prj/type3-book-prj/myprj-ts-delta/src/App.tsx
+++ tutorial-prj/type3-book-prj/myprj-ts-delta/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+// import "./custom.scss";
+import "./App.css";
+import Confirm from "./component/Confirm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React and Typescript</h1>
+      <p>This is a test only.</p>
+      <Confirm
+        title="Demo dialog"
+        content="Let's choose if want to proceed with current setting."
+      />
     </div>
   );
 }
