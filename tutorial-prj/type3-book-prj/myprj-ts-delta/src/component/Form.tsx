f51c0e38562f1def921fd2c8f31c80499636a21e
yango3 <o3.yang@hotmail.com>
2021-07-28 22:01:44 +0800

    Add form validation

--- tutorial-prj/type3-book-prj/myprj-ts-delta/src/component/Form.tsx
+++ tutorial-prj/type3-book-prj/myprj-ts-delta/src/component/Form.tsx
@@ -21,22 +21,52 @@ interface IFormProps {
   defaultValues: IValues;
   validationRules: IValidationProp;
 }
-
+interface IErrors {
+  [key: string]: string[];
+}
 interface IState {
   values: IValues;
+  errors: IErrors;
 }
 
 interface IFormContext {
+  errors: IErrors;
   values: IValues;
   setValue?: (fieldName: string, value: any) => void;
+  validate?: (fieldName: string, value: any) => void;
 }
 
-const FormContext = React.createContext<IFormContext>({ values: {} });
+const FormContext = React.createContext<IFormContext>({
+  values: {},
+  errors: {},
+});
 
 export class Form extends React.Component<IFormProps, IState> {
   private setValue = (fieldName: string, value: any) => {
     const newValues = { ...this.state.values, [fieldName]: value };
     this.setState({ values: newValues });
+  };
+  private validate = (fieldName: string, value: any): string[] => {
+    const rules = this.props.validationRules[fieldName];
+    const errors: string[] = [];
+    if (Array.isArray(rules)) {
+      rules.forEach((rule) => {
+        const error = rule.validator(fieldName, this.state.values, rule.arg);
+        if (error) {
+          errors.push(error);
+        }
+      });
+    } else {
+      if (rules) {
+        const error = rules.validator(fieldName, this.state.values, rules.arg);
+        if (error) {
+          errors.push(error);
+        }
+      }
+    }
+    const newErrors = { ...this.state.errors, [fieldName]: errors };
+    this.setState({ errors: newErrors });
+    return errors;
   };
   public static Field: React.FC<IFieldProps> = (props) => {
     const { name, label, type, options } = props;
@@ -51,6 +81,17 @@ export class Form extends React.Component<IFormProps, IState> {
         context.setValue(props.name, e.currentTarget.value);
       }
     };
+    const handleBlur = (
+      e:
+        | React.FocusEvent<HTMLInputElement>
+        | React.FocusEvent<HTMLTextAreaElement>
+        | React.FocusEvent<HTMLSelectElement>,
+      context: IFormContext
+    ) => {
+      if (context.validate) {
+        context.validate(props.name, e.currentTarget.value);
+      }
+    };
     return (
       <FormContext.Consumer>
         {(context) => (
@@ -62,6 +103,7 @@ export class Form extends React.Component<IFormProps, IState> {
                 id={name}
                 value={context.values[name]}
                 onChange={(e) => handleChange(e, context)}
+                onBlur={(e) => handleBlur(e, context)}
               />
             )}
             {type === "TextArea" && (
@@ -69,12 +111,14 @@ export class Form extends React.Component<IFormProps, IState> {
                 id={name}
                 value={context.values[name]}
                 onChange={(e) => handleChange(e, context)}
+                onBlur={(e) => handleBlur(e, context)}
               />
             )}
             {type === "Select" && (
               <select
                 value={context.values[name]}
                 onChange={(e) => handleChange(e, context)}
+                onBlur={(e) => handleBlur(e, context)}
               >
                 {options &&
                   options.map((option) => (
@@ -84,6 +128,13 @@ export class Form extends React.Component<IFormProps, IState> {
                   ))}
               </select>
             )}
+            {context.errors[name] &&
+              context.errors[name].length > 0 &&
+              context.errors[name].map((error) => (
+                <span key={error} className="form-error">
+                  {error}
+                </span>
+              ))}
           </div>
         )}
       </FormContext.Consumer>
@@ -91,14 +142,22 @@ export class Form extends React.Component<IFormProps, IState> {
   };
   public constructor(props: IFormProps) {
     super(props);
+    const errors: IErrors = {};
+    Object.keys(props.defaultValues).forEach((fieldName) => {
+      errors[fieldName] = [];
+    });
+
     this.state = {
       values: props.defaultValues,
+      errors,
     };
   }
   public render() {
     const context: IFormContext = {
+      errors: this.state.errors,
       setValue: this.setValue,
       values: this.state.values,
+      validate: this.validate,
     };
     return (
       <FormContext.Provider value={context}>
